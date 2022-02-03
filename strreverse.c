ab7b79bfc080f390918a9031a572a5939cb61386
liuvaneshka <46787523+liuvaneshka@users.noreply.github.com>
2022-02-03 18:11:06 -0300

    palabra al reves

--- strreverse.c
+++ strreverse.c
@@ -5,35 +5,26 @@
 #define MAX_CARACTER 50
 
 void invertirString(char palabra[]){
-
     int contador = 0 ;
     char invertida[MAX_CARACTER];
-
     int tamanio = strlen(palabra);  
     //printf("\n tamanio: %i",tamanio);
     
     for(int i = tamanio; i>=0; i-- ){
-
         if(palabra[i]=='e'){
-
             palabra[i] = 'a';
         }
         printf("%c",palabra[i]);
-        
         invertida[contador]=palabra[i];
         contador++;
-
     }
 
     for(int j = 0; j<=tamanio; j ++ ){
         printf("%c",invertida[j]);
         //palabra[j]=invertida[j];
     }
-
     printf("\ninvertida %s",invertida);
-
 }
-
 
 int main(){
 
