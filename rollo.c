23a74277a37a972a4882b606e7696a92380fe40f
liuvaneshka <46787523+liuvaneshka@users.noreply.github.com>
2022-02-03 18:13:32 -0300

    suma de elementos dentro de un array

--- rollo.c
+++ rollo.c
@@ -42,21 +42,18 @@ int cargar_rollos(rollo_t *rollo){
 }
 
 float calcular_peso_promedio(rollo_t rollos[MAX_ROLLS], int cantidad_rollos){
-
     float peso_total;
     float peso_prom;
 
     for(size_t i = 0; i<cantidad_rollos;i++){
         peso_total+=rollos[i].peso;
     }
-
     peso_prom= (peso_total/cantidad_rollos);
 
     return peso_prom;
 }
 
 int buscar_rollo_mayor_peso(rollo_t rollos[MAX_ROLLS],int cantidad_rollos){
-
     int nro_mayor_peso = rollos[0].nro;
     float peso_mayor = rollos[0].peso;
 
@@ -67,9 +64,7 @@ int buscar_rollo_mayor_peso(rollo_t rollos[MAX_ROLLS],int cantidad_rollos){
             printf("\n if nro mayor peso: %i",nro_mayor_peso);
             nro_mayor_peso = rollos[i].nro;
             peso_mayor = rollos[i].peso;
-
         }
-
     }
 
     printf("\nmayor peso: %f", peso_mayor);
@@ -79,7 +74,6 @@ int buscar_rollo_mayor_peso(rollo_t rollos[MAX_ROLLS],int cantidad_rollos){
 
 
 int main(){
-
     
     rollo_t rollo[MAX_ROLLS];
     int cantidad_rollos = 0;
@@ -90,10 +84,8 @@ int main(){
     peso_promedio = calcular_peso_promedio(rollo, cantidad_rollos);
     nro_rollo_mayor = buscar_rollo_mayor_peso(rollo, cantidad_rollos);
     for(size_t i = 0; i<cantidad_rollos;i++){
-
         printf("\n nro rollo: %i", rollo[i].nro);
         printf("\t peso rollo: %f", rollo[i].peso);
-
     }
     printf("\n cantidad de rollos: %i ",cantidad_rollos);
     printf("\n peso promedio: %f", peso_promedio);
